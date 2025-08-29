fe26e64ac78937ad4d3a298911801ab2c88f51e7
IzabellaSilva <izabella.silva@cinbesa.com.br>
2025-08-29 11:39:43 -0300

    feat: atualiza TesteIntegracaoUser para usar usuarios gerados em DataTesteUser nos testes

--- src/test/java/com/example/demo/TesteIntegracaoUser.java
+++ src/test/java/com/example/demo/TesteIntegracaoUser.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.List;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.web.client.TestRestTemplate;
@@ -12,7 +13,6 @@ import org.springframework.test.context.ActiveProfiles;
 
 import com.example.demo.Dto.UserDto;
 
-import io.micrometer.core.ipc.http.HttpSender.Response;
 
 @ActiveProfiles("test")
 public class TesteIntegracaoUser {
@@ -20,14 +20,15 @@ public class TesteIntegracaoUser {
     @Autowired
     TestRestTemplate testRestTemplate;
 
-    @Test
-    public void testeCreateUser() {
-        UserDto userDto = new UserDto("Fulano da Silva", "fulano1", "1234");
-
-        ResponseEntity<List<UserDto>> responseUserDto = testRestTemplate.postForEntity("/user", userDto, List.class);
-
-        assertEquals(userDto, responseUserDto);
-        assertEquals(responseUserDto.getStatusCode(), ResponseEntity.status(201).build().getStatusCode());
+    @BeforeAll
+    void setupUsers() {
+        List<UserDto> users = DataTesteUser.generateUsers();
+        
+        for (UserDto userDto : users) {
+            ResponseEntity<UserDto> responseUserDto = testRestTemplate.postForEntity("/user", userDto, UserDto.class);
+            assertEquals(responseUserDto.getStatusCode(), ResponseEntity.status(201).build().getStatusCode());
+            assertEquals(responseUserDto.getBody().getNome(), 6);
+        }
     }
 
     @Test
@@ -36,7 +37,7 @@ public class TesteIntegracaoUser {
 
         assertEquals(responseUserDto , UserDto.class);
         assertEquals(responseUserDto.getStatusCode(), ResponseEntity.status(200).build().getStatusCode());
-        assertEquals(responseUserDto.getBody().getNome(), 1);
+        assertEquals(responseUserDto.getBody().getNome(), 1 );
     }
 
     @Test
